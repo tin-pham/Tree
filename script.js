7b0cd86bb4dc20d320192f35156f5c24983187f3
PhamBaoTin_IT <heoconlinhat123@gmail.com>
2021-07-24 09:27:18 +0700

    Update

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Nav Open / Close
 function navToggle() {
   const nav = document.querySelector('nav');
   const ulList = document.querySelector('.nav-list');
@@ -13,13 +12,18 @@ function navToggle() {
     nav.classList.remove('active');
   }
 
+  // Click the menu => open nav bar
   menu.addEventListener('click', activeNavList);
+  // Click the close button => close nav bar
   close.addEventListener('click', hideNavList);
 }
 
+/*
+ * *Toggle Nav Bar Mobile Layout
+ */
 navToggle();
 
-//Header
+/* -------------------------------------- */
 
 function navColorChange() {
   const homeHeight = document.getElementById('home').getBoundingClientRect().height;
@@ -27,7 +31,9 @@ function navColorChange() {
   const header = document.querySelector('header');
 
   function headerDeal() {
-    // Nếu cuộn hết home - 120 cái thanh 'Hãy cùng tôi khám phá thì header thành trắng
+    /* * 
+    ? Nav Bar change color when the scroll height >= the initial home height - 120 (contain the about us header height, so the effect will be better) 
+     */
     if (window.scrollY >= homeHeight - 120) {
       header.classList.add('white');
     } else {
@@ -37,26 +43,25 @@ function navColorChange() {
 
   window.addEventListener('scroll', headerDeal);
 }
+/*
+ * * Color Nav Bar Toggle
 
+ */
 navColorChange();
 
-// Loader
+/* -------------------------------------- */
+
+/*
+ * * Loading Screen.
+ */
 window.addEventListener('load', () => {
   document.querySelector('.loader').classList.add('hide');
 });
 
-// Money count up when you scroll to see them
+/* -------------------------------------- */
+
 function randomNumber(min, max) {
   return Math.round(Math.random() * (max - min + 1) + min);
-}
-
-const money = document.getElementById('money');
-let target;
-if (!hasMoney()) {
-  target = randomNumber(1500, 2600);
-  localStorage.setItem('target', target);
-} else {
-  target = +localStorage.getItem('target');
 }
 
 function hasMoney() {
@@ -67,6 +72,19 @@ function hasMoney() {
   }
   return false;
 }
+
+function getTarget() {
+  let target;
+  if (!hasMoney()) {
+    target = randomNumber(1500, 2600);
+    localStorage.setItem('target', target);
+    return target;
+  }
+  return (target = +localStorage.getItem('target'));
+}
+
+const money = document.getElementById('money');
+let target = getTarget();
 
 function moneyIncrease() {
   const speed = 7;
@@ -89,32 +107,64 @@ function moneyIncrease() {
   }
 }
 
-// Scroll and trigger the counter if that inside innerHeight
+/*
+ * *  The counter (scroll)
+ */
 window.addEventListener('scroll', moneyIncrease);
-// If the initial page contain the counter, the function still trigger without scroll event
+/* 
+* * The counter
+? (If the initial page contain the counter => still trigger)
+*/
 moneyIncrease();
 
-/* Donate the money */
+/* -------------------------------------- */
+
+/*
+ * * Donate the money
+ */
 
 const submit = document.getElementById('send__money');
 const form = document.querySelector('form');
 
 function addMoney(amount) {
+  // ? Add money depend on input value
   let money = +localStorage.getItem('target');
   money += amount;
   localStorage.setItem('target', money);
 
   document.getElementById('money').innerHTML = money;
 }
+const popup = document.querySelector('.popup');
+function showPopup() {
+  // ? add the class 'show' to popup element which have been created before but hidden
+  popup.classList.add('show');
+}
+
+function closePopup() {
+  popup.classList.remove('show');
+}
+
+function addMoneyToPopUp(amount) {
+  console.log(document.getElementById('amount'));
+  document.getElementById('amount').innerHTML = amount;
+}
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
+  //  ? add money depend on which label is checked
   const inputs = document.querySelectorAll('input[type="radio"]');
   inputs.forEach((input) => {
     if (input.checked) {
-      // console.log(+input.value);
       addMoney(+input.value);
+
+      // ? Add money to PopUp
+      addMoneyToPopUp(+input.value);
     }
   });
+
+  // ? Pop up after donate
+  showPopup();
 });
+
+document.getElementById('close').addEventListener('click', closePopup);
